efda49f7e7b85202d064df56a83740fd1e26e27a
Timo Zander <timo.zander@outlook.com>
2021-10-31 02:10:04 +0200

    feat: added the budgetcard in calculations

--- src/views/calculations/index.vue
+++ src/views/calculations/index.vue
@@ -6,11 +6,12 @@
         Getränkeabrechnungen
       </div>
     </DynamicCardComponent>
-    {{ calculations }}
     <div class="d-flex justify-content-between">
-      <BudgetCard :calc="offeneKosten" />
-      <BudgetCard :calc="offeneKosten" />
-      <BudgetCard :calc="offeneKosten" />
+      <BudgetCard
+        :calc="calculation"
+        v-for="calculation in calculations"
+        :key="calculation.id"
+      />
     </div>
   </div>
 </template>
@@ -18,6 +19,8 @@
 <script>
 import DynamicCardComponent from "@/components/DynamicCardComponent";
 import BudgetCard from "@/views/calculations/components/BudgetCard";
+import { User } from "@/models";
+
 export default {
   name: "index",
   components: { BudgetCard, DynamicCardComponent },
@@ -31,8 +34,23 @@ export default {
   },
   computed: {
     calculations() {
-      return this.$store.getters["Calculations/all"];
+      return this.$store.getters["CalculationMembers/byUserId"](1);
     },
+    currentUser() {
+      const token = this.$store.getters["Login/data"];
+      if (token.userId) {
+        const user = this.$store.getters["Users/byId"](token.userId);
+
+        if (!user.id) {
+          this.$store.dispatch("Users/byId", token.userId);
+        }
+        return user;
+      }
+      return new User();
+    },
+  },
+  mounted() {
+    this.$store.dispatch("CalculationMembers/all");
   },
 };
 </script>
